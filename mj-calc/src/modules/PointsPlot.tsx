03e4603a201d7ef595c382d9966178ca5c16ccef
Kevin Hu <yhu104@aucklanduni.ac.nz>
2022-06-11 02:20:27 +1200

    Initial graph

---
+++ mj-calc/src/modules/PointsPlot.tsx
@@ -0,0 +1,99 @@
+import { Line } from "react-chartjs-2";
+import { IPlayerTable } from "./util/IPlayer";
+import { IRecord } from "./util/IRecord";
+import {
+  Chart as ChartJS,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from "chart.js";
+import { GameStatus } from "./util/GameStatus";
+import { getWind } from "./util/Wind";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: false,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
+
+export function PointsPlot({
+  players,
+  gameRecord,
+  startingPoint,
+  gameStatus,
+}: {
+  players: IPlayerTable;
+  gameRecord: IRecord[];
+  startingPoint: number;
+  gameStatus: GameStatus;
+}) {
+  const n = players.length;
+  const colors = [
+    "rgb(255,  99, 132)",
+    "rgb( 75, 192, 192)",
+    "rgb(255, 205, 86)",
+    "rgb(153, 102, 255)",
+  ];
+  const bgColors = [
+    "rgba(255,  99, 132, 0.5)",
+    "rgba( 75, 192, 192, 0.5)",
+    "rgba(255, 205, 86 , 0.5)",
+    "rgba(153, 102, 255, 0.5)",
+  ];
+  const ptsData: number[][] = Array(n)
+    .fill(0)
+    .map(() => Array(1).fill(startingPoint));
+
+  gameRecord.forEach((record, iRecord) => {
+    record.deltas.forEach((delta, iDelta) => {
+      ptsData[iDelta].push(ptsData[iDelta][iRecord] + delta);
+    });
+  });
+  console.log(ptsData);
+  console.log(gameRecord);
+  const datasets = ptsData.map((ptsData, index) => ({
+    label: players[index].name,
+    data: ptsData,
+    borderColor: colors[index],
+    backgroundColor: bgColors[index],
+  }));
+  const labels = gameRecord.map(
+    (record) => `${getWind(record.wind)}${record.round}.${record.honba}`
+  );
+  labels.push(
+    `${getWind(gameStatus.wind)}${gameStatus.round}.${gameStatus.honba}`
+  );
+  const data = {
+    labels,
+    datasets,
+  };
+  return (
+    <div className="card">
+      <div className="card-header">Final points: </div>
+      <div className="card-body">
+        <Line options={options} data={data}></Line>
+      </div>
+    </div>
+  );
+}
