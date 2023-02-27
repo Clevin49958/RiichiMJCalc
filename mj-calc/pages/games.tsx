de23b62048572d46219400291c562c07f9ad72e1
Kevin Hu <hyxkv925@gmail.com>
2023-02-28 01:08:20 +1300

    Revalidate pages on new request

--- mj-calc/pages/games.tsx
+++ mj-calc/pages/games.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { InferGetStaticPropsType } from "next";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import GameList from "../src/modules/components/GameList";
 import { getAllGames } from "../src/library/getAllGames";
 
@@ -9,11 +9,12 @@ export default function GameListPage({
   return <GameList games={games} />;
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const games = await getAllGames();
   return {
     props: {
       games,
     },
+    revalidate: 10,
   };
-}
+};
