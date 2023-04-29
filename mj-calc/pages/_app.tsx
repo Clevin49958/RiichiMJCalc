68a47289272f823911a0be3ab00d3fba96de3e6a
Kevin Hu <hyxkv925@gmail.com>
2023-04-30 03:24:07 +1200

    Add vercel analytics

--- mj-calc/pages/_app.tsx
+++ mj-calc/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { AppProps } from "next/app";
 import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Analytics } from "@vercel/analytics/react";
 import "./_app.css";
 import "./_index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
