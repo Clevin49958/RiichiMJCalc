459dc1244cd571bebdb94259fbfd2c61398026d1
Kevin Hu <hyxkv925@gmail.com>
2023-05-01 01:35:07 +1200

    Add i18n

--- mj-calc/next.config.js
+++ mj-calc/next.config.js
@@ -1,13 +1,25 @@
 // next.config.js
 module.exports = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en-US", "zh-CN"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "en-US",
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    domains: [],
+  },
   experimental: {
     swcPlugins: [
       [
-        'next-superjson-plugin',
+        "next-superjson-plugin",
         {
           excluded: [],
         },
       ],
     ],
   },
-}
+};
